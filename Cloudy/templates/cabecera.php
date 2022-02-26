fcb7dfd478d5ca40399b8c6a3440fee5e531a3e9
DanyMelguizo <73504424+Danymelguizo@users.noreply.github.com>
2022-02-25 21:36:12 -0500

    Add files via upload

---
+++ Cloudy/templates/cabecera.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang='es'>
+    <head>
+        <meta charset="utf-8">
+        <title>Cloudy</title>
+        <link rel="stylesheet" href="css/c.css">
+        <link rel="icon" href="icos/Cloudy.ico">
+        <script src="https://kit.fontawesome.com/cc21c1d6c9.js" crossorigin="anonymous"></script>
+        <meta name="robots" content="noindex">
+
+    </head>
+    <body>
+        <header>
+            <img id="cloudylogo" src="icos/cloudy.png" tittle="Cloudy-Logo">
+            <h1 class="cabeza">Cloudy</h1>
+            <input class="search" type="text" autocomplete="off" placeholder="Buscar archivo">
+            <button class="boton-ajustes">
+                <img id="icono-ajustes"src="icos/menu-hamburguesa.png" title="Ajustes">
+            </button>
+        </header>
+        
+        <div class="menu">
+            <aside>
+                <nav>
+                    <ul class="nav_ul">
+                        <li class="nav_li allfiles"><a class="seccion" href="index.php">Todos los archivos</a></li>
+                        <li class="nav_li images"><a class="seccion" href="images.php">Imagenes</a></li>
+                        <li class="nav_li videos"><a class="seccion" href="videos.php">Videos</a></li>
+                        <li class="nav_li files"><a class="seccion" href="files.php">Documentos</a></li>
+                        <li class="nav_li bin"><a class="seccion" href="bin.php">Papelera</a></li>
+                    </ul>
+                </nav>
+            </aside>
+        </div>
